9a781592b3faa63a3840113ab09af7b7b0fbafea
David Štěpánek <dstepanek@profinit.eu>
2022-05-25 11:36:40 +0200

    Performing update operations

--- APM-Demo0/src/app/products/state/product.actions.ts
+++ APM-Demo0/src/app/products/state/product.actions.ts
@@ -5,7 +5,7 @@ export const toggleProductCode = createAction('[Product] Toggle Product Code');
 
 export const setCurrentProduct = createAction(
   '[Product] Set Current Product',
-  props<{ product: Product }>()
+  props<{ currentProductId: number }>()
 );
 
 export const clearCurrentProduct = createAction(
@@ -25,5 +25,50 @@ export const loadProductsSuccess = createAction(
 
 export const loadProductsFailure = createAction(
   '[Product] Load Fail',
+  props<{ error: string }>()
+);
+
+export const updateProduct = createAction(
+  '[Product] Update Product',
+  props<{ product: Product }>()
+);
+
+export const updateProductSuccess = createAction(
+  '[Product] Update Product Success',
+  props<{ product: Product }>()
+);
+
+export const updateProductFailure = createAction(
+  '[Product] Update Product Fail',
+  props<{ error: string }>()
+);
+
+export const createProduct = createAction(
+  '[Product] Create Product',
+  props<{ product: Product }>()
+);
+
+export const createProductSuccess = createAction(
+  '[Product] Create Product Success',
+  props<{ product: Product }>()
+);
+
+export const createProductFailure = createAction(
+  '[Product] Create Product Fail',
+  props<{ error: string }>()
+);
+
+export const deleteProduct = createAction(
+  '[Product] Delete Product',
+  props<{ productId: number }>()
+);
+
+export const deleteProductSuccess = createAction(
+  '[Product] Delete Product Success',
+  props<{ productId: number }>()
+);
+
+export const deleteProductFailure = createAction(
+  '[Product] Delete Product Fail',
   props<{ error: string }>()
 );
